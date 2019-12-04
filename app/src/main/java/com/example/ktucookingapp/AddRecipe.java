d6b212698260a0246ef845235757c851306d9697
Said-Yoga <said.lachhab@t-online.de>
2019-12-04 05:34:31 +0200

    Ende Lab2+ layout recipes+ layout addRecipe

--- app/src/main/java/com/example/ktucookingapp/AddRecipe.java
+++ app/src/main/java/com/example/ktucookingapp/AddRecipe.java
@@ -8,13 +8,20 @@ import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.Scroller;
+import android.widget.Spinner;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AddRecipe extends AppCompatActivity {
 
@@ -22,6 +29,8 @@ public class AddRecipe extends AppCompatActivity {
     ImageButton uploadImage;
     Button addRecipe;
     EditText editInstructions;
+    Spinner spinner;
+    ListView listViewIngredients;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +46,8 @@ public class AddRecipe extends AppCompatActivity {
         uploadImage = (ImageButton) findViewById(R.id.recipeImage);
         addRecipe = (Button) findViewById(R.id.btnAddRecipe);
         addRecipe.setOnClickListener(addRecipeClicked);
+
+        //Makes the edittext for the instructions scrollable
         editInstructions = (EditText) findViewById(R.id.etInstructions);
         editInstructions.setOnTouchListener(new View.OnTouchListener() {
             @Override
@@ -52,6 +63,31 @@ public class AddRecipe extends AppCompatActivity {
                 return false;
             }
         });
+
+
+        //Spinner for the difficulty
+        spinner = (Spinner) findViewById(R.id.spinnerDifficulty);
+        final List<String> difficulties = new ArrayList<>();
+        difficulties.add(0,"Choose meals difficulty");
+        difficulties.add("My dog can do it");
+        difficulties.add("easy");
+        difficulties.add("medium");
+        difficulties.add("hard");
+        difficulties.add("For boss cookers only");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, difficulties);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        //Listview for the ingredients
+        listViewIngredients = (ListView) findViewById(R.id.listViewIngredients);
+
+        List<String> addedIngredients = new ArrayList<>();
+        addedIngredients.add("test");
+        addedIngredients.add("hiii");
+        ArrayAdapter<String> adapterIngredients = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, addedIngredients);
+        //ArrayAdapter<String> adapterIngredients = new ArrayAdapter<String>(this, R.layout.listitem_ingredient, addedIngredients);
+        listViewIngredients.setAdapter(adapterIngredients);
     }
 
 
